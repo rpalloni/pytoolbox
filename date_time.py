0088265522b5ac862c4741fb30f0396f5e90b78b
rpalloni <rpdev20@gmail.com>
2021-06-21 08:59:11 +0200

    time

---
+++ date_time.py
@@ -0,0 +1,31 @@
+import time
+time.time() # number of seconds since the Unix epoch 01/01/1970 00:00:00 GMT Greenwich Mean Time
+
+from datetime import date, time, datetime
+date(year=2020, month=1, day=31)
+
+time(hour=13, minute=14, second=31)
+
+datetime(year=2020, month=1, day=31, hour=13, minute=14, second=31)
+
+today = date.today()
+today
+
+now = datetime.now()
+now
+
+current_time = time(now.hour, now.minute, now.second)
+datetime.combine(today, current_time)
+
+# ISO: all dates should be written in order of most-to-least-significant data YYYY-MM-DD HH:MM:SS
+date.fromisoformat("2020-01-31")
+
+date_string = "01-31-2020 14:45:37"
+format_string = "%m-%d-%Y %H:%M:%S"
+datetime.strptime(date_string, format_string)
+
+today.strftime("%Y-%m-%d")
+today.strftime("%d-%m-%Y")
+
+# django template date format [db: yyyy-mm-dd]
+#  <input type="date" class="form-control" placeholder="End year" name="session_end" value="{{ student.session_end|date:"Y-m-d" }}">
