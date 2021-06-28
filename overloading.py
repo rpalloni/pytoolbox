692313c98bce6cd0b008d1a34f7fd4594eec4f7e
rpalloni <rpdev20@gmail.com>
2021-06-28 21:50:36 +0200

    operators

--- overloading.py
+++ overloading.py
@@ -146,3 +146,48 @@ order = Order(['banana', 'apple', 'orange', 'mango'], 'Joe')
 order[0]
 order[-1]
 order[2:]
+
+
+'''
+Basic Operators :
++	__add__(self, other)
+–	__sub__(self, other)
+*	__mul__(self, other)
+/	__truediv__(self, other)
+//	__floordiv__(self, other)
+%	__mod__(self, other)
+**	__pow__(self, other)
+>>	__rshift__(self, other)
+<<	__lshift__(self, other)
+&	__and__(self, other)
+|	__or__(self, other)
+^	__xor__(self, other)
+
+Comparison Operators :
+<	__lt__(self, other)
+>	__gt__(self, other)
+<=	__le__(self, other)
+>=	__ge__(self, other)
+==	__eq__(self, other)
+!=	__ne__(self, other)
+
+Assignment Operators :
+-=	__isub__(self, other)
++=	__iadd__(self, other)
+*=	__imul__(self, other)
+/=	__idiv__(self, other)
+//=	__ifloordiv__(self, other)
+%=	__imod__(self, other)
+**=	__ipow__(self, other)
+>>=	__irshift__(self, other)
+<<=	__ilshift__(self, other)
+&=	__iand__(self, other)
+|=	__ior__(self, other)
+^=	__ixor__(self, other)
+
+Unary Operators :
+–	__neg__(self, other)
++	__pos__(self, other)
+~	__invert__(self, other)
+
+'''
