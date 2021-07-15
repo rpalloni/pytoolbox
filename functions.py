b9ca77084f8b4190e3d724d1bb2e613bfaf4885d
rpalloni <rpdev20@gmail.com>
2021-07-15 22:14:08 +0200

    example

--- functions.py
+++ functions.py
@@ -16,3 +16,22 @@ sum_many(1, 2, 3, 4, 5, 7, 8, 9, 10)
 def profile(**kwargs):
     for key, value in kwargs.items():
         print(key, ":", value)
+
+# args and kwargs
+def cheeseshop(kind, *arguments, **keywords):
+    print("-> Do you have any", kind, "?")
+    print("-> I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("<->" * 10)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger")
+
+cheeseshop("Limburger",
+            "It's very runny, sir.",
+            "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
