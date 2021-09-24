62c27abc50b2bf06b4e585e37996fb4ed75d4d25
Roberto Palloni <rpdev20@gmail.com>
2021-09-24 13:52:35 +0200

    with type hints

---
+++ with_type_hints.py
@@ -0,0 +1,32 @@
+# type hints: modern python feature
+
+from collections import namedtuple
+from typing import Optional, Iterable
+
+# https://docs.python.org/3/library/collections.html#collections.namedtuple
+Order = namedtuple('Order', 'item, price')
+
+
+def get_cost(orders: Iterable[Order]) -> float:
+  ''' type hints: function gets an iterable of order objects and returns a float '''
+    total = 0
+
+    for o in orders:
+        total += o.price
+
+    return total
+
+
+  
+def main() -> None:
+    # iterables of order objects
+    dinner = [Order('Pizza', 20.5), Order('Beer', 9.8), Order('Beer', 9.8)]
+    breakfast = [Order('Pancakes', 11.3), Order('Bacon', 4), Order('Coffee', 3.6), Order('Coffee', 3.6)]
+
+    print(f"Dinner cost EUR {get_cost(dinner):,.02f}")
+    print(f"Breakfast cost EUR {get_cost(breakfast):,.02f}")
+
+    
+
+if __name__ == '__main__':
+    main()
