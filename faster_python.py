b4e2099696f579d7f4c8ca6a9736fcebe6d6a005
Roberto Palloni <rpdev20@gmail.com>
2021-11-03 18:10:08 +0100

    faster code structures

--- faster_python.py
+++ faster_python.py
@@ -1,80 +1,169 @@
 import time
+import random
+
+n = 1000000
+rng = range(1, n)
+nr = [random.randint(0, 9) for i in rng]
 
 # faster algorithms
+
+start = time.time()
 sum = 0
-for x in range(1,100+1):
+for x in range(1, n+1):
     sum += x
 print(sum)
+print(time.time() - start)
 
-s = sum(range(1,100+1))
-print(s)
+start = time.time()
+print(n*(1+n)/2)
+print(time.time() - start)
 
-print(100*(1+100)/2)
 
-# faster python (banning for loops)
+# faster python (code structures)
 
-#1 count elements in list
+### 0 - create list/dict
+start = time.time()
+l1 = list() # dict()
+print(time.time() - start)
+
+start = time.time()
+l2 = [] # {}
+print(time.time() - start)
+
+### 1 - count elements in list
+start = time.time()
 how_many = 0
-for element in ONE_MILLION_ELEMENTS:
+for element in rng:
   how_many += 1
 print(how_many)
+print(time.time() - start)
 
-print(len(ONE_MILLION_ELEMENTS))
+start = time.time()
+print(len(rng))
+print(time.time() - start)
 
-#2 filter a list
+### 2 - filter a list
+start = time.time()
 output = []
-for element in MILLION_NUMBERS:
+for element in rng:
   if element % 2:
     output.append(element)
-    
-list(filter(lambda x: x % 2, MILLION_NUMBERS))
+print(time.time() - start)
 
-[item for item in MILLION_NUMBERS if item % 2]
-  
-#3 item in list
+start = time.time()
+list(filter(lambda x: x % 2, rng))
+print(time.time() - start)
+
+start = time.time()
+[item for item in rng if item % 2]
+print(time.time() - start)
+
+### 3 - item in list
 def check_number(number):
-    for item in MILLION_NUMBERS:
+    for item in rng:
         if item == number:
             return True
-        return False
-'''    
-speed depends on item position => no constant lookup time
-100 in MILLION_NUMBERS
-100000 in MILLION_NUMBERS 
-'''
+    return False
 
-MILLION_SET = set(MILLION_NUMBERS) # set has constant lookup time
-100 in MILLION_SET
-100000 in MILLION_SET
+start = time.time()
+check_number(125)
+print(time.time() - start)
 
-#4 remove duplicates
+start = time.time()
+check_number(99999) # speed depends on item position => no constant lookup time
+print(time.time() - start)
+
+# use set for constant lookup time
+MILLION_SET = set(rng)
+
+start = time.time()
+125 in MILLION_SET
+print(time.time() - start)
+
+start = time.time()
+99999 in MILLION_SET
+print(time.time() - start)
+
+### 4 - remove duplicates
+start = time.time()
 unique = []
-for item in MILLION_ELEMENTS:
+for item in nr:
     if item not in unique:
         unique.append(item)
-       
-set(MILLION_ELEMENTS) # set conversion removes duplicates
+print(time.time() - start)
 
-#5 list sorting
-sorted(MILLION_RANDOM_NUMBERS)
+start = time.time()
+set(nr) # set conversion removes duplicates
+print(time.time() - start)
 
-MILLION_RANDOM_NUMBERS.sort()
+### 5 - list sorting
+start = time.time()
+sorted(nr)
+print(time.time() - start)
 
-#6 apply function
+start = time.time()
+nr.sort()
+print(time.time() - start)
+
+### 6 - apply function
 def square(number):
     return number**2
-squares = [square(i) for i in range(1000)] # call function i times
+
+start = time.time()
+squares = [square(i) for i in rng] # call function i times
+print(time.time() - start)
 
 def squares():
-    return [i**2 for i in range(1000)] # call once
+    return [i**2 for i in rng] # call once
 
-#7 check truty
+start = time.time()
+squares()
+print(time.time() - start)
+
+### 7 - function vs lambda
+def greet(name):
+    return f'Hello {name}'
+
+start = time.time()
+greet('John')
+print(time.time() - start)
+
+lgreet = lambda name: f'Hello {name}'
+
+start = time.time()
+lgreet('John')
+print(time.time() - start)
+
+### 8 - check truthy
 variable = True
+
+start = time.time()
 if variable == True:
-    print('Hello')
-    
+    print('hello')
+print(time.time() - start)
+
+start = time.time()
 if variable is True:
-    print('Hello')
-    
+    print('hello')
+print(time.time() - start)
+
+start = time.time()
 if variable:
-    print('Hello')
+    print('hello')
+print(time.time() - start)
+
+# falsy (False, None, zero, empty string, empty list)
+start = time.time()
+if len(nr) == 0:
+    print('hello')
+print(time.time() - start)
+
+start = time.time()
+if nr == []:
+    print('hello')
+print(time.time() - start)
+
+start = time.time()
+if not nr:
+    print('hello')
+print(time.time() - start)
